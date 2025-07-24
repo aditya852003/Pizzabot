66563e57f094cc852c35e3f061ed9e5cf04f70c3
Aditya Kumar <149161513+aditya852003@users.noreply.github.com>
2025-07-24 23:08:18 +0530

    Update main.py
    Intendation correction

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import openai
 from dotenv import load_dotenv, find_dotenv_ = load_dotenv(find_dotenv()) # read local .env file
 
+
+
+
 openai.api_key  = os.getenv('OPENAI_API_KEY')
 
 
@@ -10,20 +13,18 @@ openai.api_key  = os.getenv('OPENAI_API_KEY')
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
-    )
+    response = openai.ChatCompletion.create( model=model,
+                                            messages=messages, 
+                                            temperature=0, # this is the degree of randomness of the model's output 
+                                           )
     return response.choices[0].message["content"]
 
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature, # this is the degree of randomness of the model's output
-    )
-#     print(str(response.choices[0].message))
+    response = openai.ChatCompletion.create(model=model,
+                                            messages=messages,
+                                            temperature=temperature, # this is the degree of randomness of the model's output
+                                           )
+    #     print(str(response.choices[0].message))
     return response.choices[0].message["content"]
 
 
@@ -37,11 +38,8 @@ def collect_messages(_):
     context.append({'role':'user', 'content':f"{prompt}"})
     response = get_completion_from_messages(context) 
     context.append({'role':'assistant', 'content':f"{response}"})
-    panels.append(
-        pn.Row('User:', pn.pane.Markdown(prompt, width=600)))
-    panels.append(
-        pn.Row('Assistant:', pn.pane.Markdown(response, width=600, style={'background-color': '#F6F6F6'})))
- 
+    panels.append(pn.Row('User:', pn.pane.Markdown(prompt, width=600)))
+    panels.append(pn.Row('Assistant:', pn.pane.Markdown(response, width=600, style={'background-color': '#F6F6F6'}))) 
     return pn.Column(*panels)
 
 
@@ -89,11 +87,10 @@ button_conversation = pn.widgets.Button(name="Chat!")
 
 interactive_conversation = pn.bind(collect_messages, button_conversation)
 
-dashboard = pn.Column(
-    inp,
-    pn.Row(button_conversation),
-    pn.panel(interactive_conversation, loading_indicator=True, height=300),
-)
+dashboard = pn.Column(inp,
+                      pn.Row(button_conversation),
+                      pn.panel(interactive_conversation, loading_indicator=True, height=300),
+                     )
 dashboard
 
 
